a4b8809ee76d73201f144386be6fe6bc63517ab0
Naveen Reddy <naveenreddyin@gmail.com>
2015-10-22 16:10:44 +0200

    Changed routes and action names and added save method (cherry picked from commit f8c9d59)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,20 +2,27 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import scala.slick.driver.MySQLDriver.simple._
 
 import assets.getForms._
 
+import scala.slick.jdbc.meta.MTable
+
 object Application extends Controller {
+  // The query interface for the Suppliers table
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready...."))
+
+    Redirect(routes.Application.company())
   }
 
   def company = Action {
     Ok(views.html.addcompany("Please register your company.", addCompanyForm))
   }
 
-  def saveCompany = Action{
+  def saveCompany = Action{ implicit request =>
+    val (name, address) = addCompanyForm.bindFromRequest.get
+    Logger.debug(name)
     Ok("success")
   }
 
